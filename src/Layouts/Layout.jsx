36b4085e6d94d5f76286751c5e08cbee8cfdd833
UmutCan <81829534+Umutc2171@users.noreply.github.com>
2024-10-10 14:19:39 +0300

    de

--- src/Layouts/Layout.jsx
+++
@@ -1,6 +0,0 @@
-import MainLayout from "./MainLayout";
-import AdminLayout from "./AdminLayout";
-
-const inAdmin = window.location.pathname.startsWith("/admin")
-
-export const Layout = inAdmin ? AdminLayout : MainLayout;
